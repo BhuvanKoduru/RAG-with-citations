3920e5de189e1fc0aeaf8307973b9874510a6320
Bhuvan-CV <bhuvan@euphotic.io>
2024-05-22 19:20:51 +0530

    Adding necessary files
    Adding necessary files

---
+++ multipage/Homepage.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from streamlit import session_state as ss
+import streamlit as st
+import json
+import os
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredHTMLLoader,PyPDFLoader
+import shutil
+from langchain_community.vectorstores import FAISS
+import os
+import time
+from langchain_community.vectorstores import Chroma
+
+if('db_created' not in st.session_state):
+    st.session_state['db_created']=False
+if('subject' not in st.session_state):
+    st.session_state['subject']=None
+if('grade' not in st.session_state):
+    st.session_state['grade']=None
+   
+st.title("History and Economics Chatbot")
+
+
+subject = st.selectbox("Select Subject", ["Economics", "History"],index=0)
+
+if subject == "Economics":
+    grade = st.selectbox("Select Grade", ["11th", "12th"],index=0)
+else:
+    grade = st.selectbox("Select Grade", ["10th", "11th", "12th"],index=0)
+
+if(st.button("Create Database")):
+    folder_path = f"D:\\ML\\superkalam\\database\\{subject}\\{grade}"
+    if os.path.exists(folder_path):
+        st.success(f"The database folder for {subject} {grade} grade already exists.")
+    else:
+        with st.spinner("Creating database folder..."):
+            # time.sleep(3)  
+            loader=DirectoryLoader(f"D:\\ML\\superkalam\\docs\\{subject}\\{grade}",loader_cls=PyPDFLoader)
+            documents = loader.load()
+            text_splitter = CharacterTextSplitter(chunk_size=1024, chunk_overlap=128)
+            docs = text_splitter.split_documents(documents)
+            embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+            # FAISS
+            db = FAISS.from_documents(docs, embedding_function)
+            db.save_local(f"D:\\ML\\superkalam\\database\\{subject}\\{grade}")
+            # Chroma
+            # db = Chroma.from_documents(docs, embedding_function,persist_directory='db')
+            # db = Chroma(persist_directory=f"D:\\ML\\superkalam\\database_chroma\\{st.session_state['subject']}\\{st.session_state['grade']}", embedding_function=embedding_function)
+            st.success(f"The database folder for {subject} {grade} has been created.")
+    st.session_state['db_created']=True
+    st.session_state['subject']=subject
+    st.session_state['grade']=grade
+
---
+++ multipage/pages/Query Mistral.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+
+from streamlit_pdf_viewer import pdf_viewer
+def view_pdf(file,page):
+    st.header(file.split("\\")[-1]+ " Page number "+str(page+1))
+    pdf_viewer(file,pages_to_render=[page+1])
+
+st.title("Mistral Bot")
+if "db_created" not in st.session_state:
+        st.session_state["db_created"] = False
+if not st.session_state['db_created']:
+    st.warning("Please ensure the database has been created and loaded before querying")
+else:
+    if "messages_mistral" not in st.session_state:
+        st.session_state.messages_mistral = []
+
+# Display chat messages from history on app rerun
+    for message in st.session_state.messages_mistral:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if user_query := st.chat_input("Enter your queries here"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(user_query)
+        # Add user message to chat history
+        st.session_state.messages_mistral.append({"role": "user", "content": user_query})
+        with st.spinner('Generating answer...'):
+    
+            embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+           #FAISS
+            db=FAISS.load_local(f"D:\\ML\\superkalam\\database\\{st.session_state['subject']}\\{st.session_state['grade']}", embedding_function,allow_dangerous_deserialization=True)
+            #Chroma
+            # db = Chroma(persist_directory=f"D:\\ML\\superkalam\\database_chroma\\{st.session_state['subject']}\\{st.session_state['grade']}", embedding_function=embedding_function)
+            docs_faiss = db.similarity_search(user_query)
+            llm = Ollama(model='mistral')
+            chain = load_qa_chain(llm, chain_type="stuff")
+            answer = chain.run(input_documents=docs_faiss, question=user_query) 
+        with st.chat_message("assistant"):
+            st.markdown(answer + " The sources have been shown below:")
+        # Add assistant response to chat history
+        for doc in docs_faiss:
+            view_pdf(doc.metadata['source'], doc.metadata['page'])
+        # Add assistant response to chat history
+        st.session_state.messages_mistral.append({"role": "assistant", "content": answer})
